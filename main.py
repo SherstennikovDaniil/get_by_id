99f167e64c026cdfa49af1d2c8e31b49509df937
Даниил Шерстенников <70951451+SherstennikovDaniil@users.noreply.github.com>
2020-10-09 22:27:37 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+from TikTokApi import TikTokApi
+import sqlite3
+
+
+api = TikTokApi()
+db = sqlite3.connect('data.db')
+cr = db.cursor()
+
+
+def get_by_nick(id):
+    try:
+        data = api.getUser(id)
+        uid = data["userInfo"]["user"]["uniqueId"]
+        nick = data["userInfo"]["user"]["nickname"]
+        subs = data["userInfo"]["stats"]["followerCount"]
+        likes = data["userInfo"]["stats"]["heartCount"]
+        link = "https://www.tiktok.com/@" + id
+        cr.execute(f"SELECT id FROM table1 WHERE id = '{uid}'")
+        if cr.fetchone() is None:
+            cr.execute(f"INSERT INTO table1 VALUES (?, ?, ?, ?, ?)", (uid, nick, subs, likes, link))
+            db.commit()
+            print("Сделали ", id)
+        else:
+            print(id, 'уже есть.')
+    except:
+        print(id, '-хуесос.')
+
+
+
+list = []
+with open("id_list.txt", "r", encoding='utf-8') as txt:
+    for line in txt:
+        if "Username" in line:
+            a = line.split(':')
+            b = [str(a[1]).strip()]
+            list = list + b
+
+
+if __name__ == "__main__":
+    for id in list:
+        get_by_nick(id)
